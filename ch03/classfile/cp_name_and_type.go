ecdae6b784716ae2d8fb7120547190002573a58d
gwr0-0 <win101wd@hotmail.com>
2018-09-20 19:14:15 +0800

    增加注释说明

--- ch03/classfile/cp_name_and_type.go
+++ ch03/classfile/cp_name_and_type.go
@@ -9,6 +9,9 @@ CONSTANT_NameAndType_info {
 The tag item has the value CONSTANT_NameAndType (12).
 The value of the name_index item must be a valid index into the constant_pool table.
 The value of the descriptor_index item must be a valid index into the constant_pool table.
+
+方法重载（overload），不同方法可以有相同的名字，只要参数列表不同即可。
+同理，理论上字段也可实现重载，这里是java语法的限制
 */
 
 type ConstantNameAndTypeInfo struct {

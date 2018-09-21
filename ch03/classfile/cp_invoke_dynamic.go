10238c0ced49c4ec4da27263c3bd829be32330cb
gwr0-0 <win101wd@hotmail.com>
2018-09-21 16:48:50 +0800

    常量池

---
+++ ch03/classfile/cp_invoke_dynamic.go
@@ -0,0 +1,22 @@
+package classfile
+
+/**
+CONSTANT_MethodType_info {
+    u1 tag;
+    u2 descriptor_index;
+}
+
+CONSTANT_MethodHandle_info {
+    u1 tag;
+    u1 reference_kind;
+    u2 reference_index;
+}
+
+CONSTANT_MethodType_info {
+    u1 tag;
+    u2 descriptor_index;
+}
+
+@see https://docs.oracle.com/javase/specs/jvms/se10/html/jvms-4.html#jvms-4.4.8
+TODO java7添加，为了支持invokedynnamic指令
+*/

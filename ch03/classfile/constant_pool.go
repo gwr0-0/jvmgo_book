19c1e759ae97edafa490e1bc8efbf9ae6f6529b7
gwr0-0 <win101wd@hotmail.com>
2018-09-26 16:46:35 +0800

    补完常量池方法

--- ch03/classfile/constant_pool.go
+++ ch03/classfile/constant_pool.go
@@ -34,12 +34,16 @@ func (self ConstantPool) getConstantInfo(index uint16) ConstantInfo {
 
 // 从常量池查找字段或方法的名字和描述符
 func (self ConstantPool) getNameAndType(index uint16) (string, string) {
-
+	ntInfo := self.getConstantInfo(index).(*ConstantNameAndTypeInfo)
+	name := self.getUtf8(ntInfo.nameIndex)
+	_type := self.getUtf8(ntInfo.descriptorIndex)
+	return name, _type
 }
 
 // 从常量池查找类名
 func (self ConstantPool) getClassName(index uint16) string {
-
+	classInfo := self.getConstantInfo(index).(*ConstantClassInfo)
+	return self.getUtf8(classInfo.nameIndex)
 }
 
 // 从常量池查找UTF-8字符串

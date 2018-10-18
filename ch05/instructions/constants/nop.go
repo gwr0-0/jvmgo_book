7a432fd943ce8abdc3c06f2a2035f73a5be1694a
gwr0-0 <win101wd@hotmail.com>
2018-10-18 18:06:11 +0800

    nop指令

--- ch05/instructions/constants/nop.go
+++ ch05/instructions/constants/nop.go
@@ -1,1 +1,28 @@
 package constants
+
+import (
+	"github.com/gwr0-0/jvmgo/ch05/instructions/base"
+	"github.com/gwr0-0/jvmgo/ch05/rtda"
+)
+
+/**
+Operation
+	Do nothing
+Format
+	nop
+Forms
+	nop = 0 (0x0)
+Operand Stack
+	No change
+Description
+	Do nothing.
+*/
+
+// Do nothing
+type NOP struct {
+	base.NoOperandsInstruction
+}
+
+func (self *NOP) Execute(frame *rtda.Frame) {
+
+}

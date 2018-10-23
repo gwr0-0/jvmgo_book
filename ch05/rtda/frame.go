ecdb2a09d8b3636c5877e2f66814b2cf6061b537
gwr0-0 <win101wd@hotmail.com>
2018-10-24 02:37:47 +0800

    NewFrame

--- ch05/rtda/frame.go
+++ ch05/rtda/frame.go
@@ -4,10 +4,13 @@ type Frame struct {
 	lower        *Frame        // 用来实现链表数据结构
 	localVars    LocalVars     // 局部变量表指针
 	operandStack *OperandStack // 操作数栈指针
+	thread       *Thread
+	nextPC       int
 }
 
-func NewFrame(maxLocals, maxStack uint) *Frame {
+func NewFrame(thread *Thread, maxLocals, maxStack uint) *Frame {
 	return &Frame{
+		thread:       thread,
 		localVars:    newLocalVars(maxLocals),   // 保存局部变量表指针
 		operandStack: newOperandStack(maxStack), // 保存操作数栈指针
 	}

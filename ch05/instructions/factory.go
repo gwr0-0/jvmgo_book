338d2ab24dc3279f3e1e3614df01def0e863d62b
gwr0-0 <win101wd@hotmail.com>
2018-10-27 01:21:23 +0800

    load部分指令完成（0x15~0x2d，缺0x2e~0x35）

--- ch05/instructions/factory.go
+++ ch05/instructions/factory.go
@@ -29,8 +29,31 @@ var (
 	bipush      = &BIPUSH{}
 	sipush      = &SIPUSH{}
 
+	iload   = &ILOAD{}
+	lload   = &LLOAD{}
+	fload   = &FLOAD{}
+	dload   = &DLOAD{}
+	aload   = &ALOAD{}
+	iload_0 = &ILOAD_0{}
 	iload_1 = &ILOAD_1{}
 	iload_2 = &ILOAD_2{}
+	iload_3 = &ILOAD_3{}
+	lload_0 = &LLOAD_0{}
+	lload_1 = &LLOAD_1{}
+	lload_2 = &LLOAD_2{}
+	lload_3 = &LLOAD_3{}
+	fload_0 = &FLOAD_0{}
+	fload_1 = &FLOAD_1{}
+	fload_2 = &FLOAD_2{}
+	fload_3 = &FLOAD_3{}
+	dload_0 = &DLOAD_0{}
+	dload_1 = &DLOAD_1{}
+	dload_2 = &DLOAD_2{}
+	dload_3 = &DLOAD_3{}
+	aload_0 = &ALOAD_0{}
+	aload_1 = &ALOAD_1{}
+	aload_2 = &ALOAD_2{}
+	aload_3 = &ALOAD_3{}
 
 	istore_1 = &ISTORE_1{}
 	istore_2 = &ISTORE_2{}
@@ -78,10 +101,56 @@ func NewInstruction(opcode byte) base.Instruction {
 	case 0x11:
 		return sipush
 
+	case 0x15:
+		return iload
+	case 0x16:
+		return lload
+	case 0x17:
+		return fload
+	case 0x18:
+		return dload
+	case 0x19:
+		return aload
+	case 0x1a:
+		return iload_0
 	case 0x1b:
 		return iload_1
 	case 0x1c:
 		return iload_2
+	case 0x1d:
+		return iload_3
+	case 0x1e:
+		return lload_0
+	case 0x1f:
+		return lload_1
+	case 0x20:
+		return lload_2
+	case 0x21:
+		return lload_3
+	case 0x22:
+		return fload_0
+	case 0x23:
+		return fload_1
+	case 0x24:
+		return fload_2
+	case 0x25:
+		return fload_3
+	case 0x26:
+		return dload_0
+	case 0x27:
+		return dload_1
+	case 0x28:
+		return dload_2
+	case 0x29:
+		return dload_3
+	case 0x2a:
+		return aload_0
+	case 0x2b:
+		return aload_1
+	case 0x2c:
+		return aload_2
+	case 0x2d:
+		return aload_3
 
 	case 0x3c:
 		return istore_1
